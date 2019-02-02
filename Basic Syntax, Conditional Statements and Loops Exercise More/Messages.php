f91675a6da36ed897cb4afdfebf6ab7d826dd3bb
plezzz <nikid12e@gmail.com>
2019-02-02 18:34:30 +0200

    Update Messages.php

--- Basic Syntax, Conditional Statements and Loops Exercise More/Messages.php
+++ Basic Syntax, Conditional Statements and Loops Exercise More/Messages.php
@@ -2,7 +2,25 @@
 /**
  * Created by PhpStorm.
  * User: Niki
- * Date: 28/01/2019
- * Time: 16:08
+ * Date: 02/02/2019
+ * Time: 16:56
  */
+$cycle =readline();
+$string="";
+for ($i=1;$i<=$cycle;$i++){
 
+    $number = readline();
+    $numberOfDigits = strlen($number);
+    $mainDigit = substr($number, $numberOfDigits - 1);
+    $offset = ($mainDigit - 2) * 3;
+    if ($mainDigit == 8 || $mainDigit == 9) {
+        $letterIndex = $offset + $numberOfDigits;
+    }elseif ($mainDigit == 0){
+        $letterIndex = -65;
+    }else{
+        $letterIndex = $offset + $numberOfDigits - 1;
+    }
+    $str = chr(97 + $letterIndex);
+    $string.=$str;
+}
+echo $string;
